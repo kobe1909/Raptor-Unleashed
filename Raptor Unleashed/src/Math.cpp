5d6b33dfc6ae96da4b64e30149246107d4cb7756
kobe1909 <kobe.vdstappen@gmail.com>
2023-11-02 22:36:37 +0100

    changed mesh

---
+++ Raptor Unleashed/src/Math.cpp
@@ -0,0 +1,14 @@
+#include "Math.h"
+
+Vector2::Vector2(float x, float y) {
+	this->x = x;
+	this->y = y;
+}
+
+
+
+Vector3::Vector3(float x, float y, float z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
--- Raptor Unleashed/src/Raptor Unleashed.cpp
+++ Raptor Unleashed/src/Raptor Unleashed.cpp
@@ -1,7 +1,12 @@
 // Raptor Unleashed.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "renderer.h"
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "Renderer.h"
 
 int main(void) {
 	Renderer renderer;
@@ -10,7 +15,16 @@ int main(void) {
 	if (!glfwInit())
 		return -1;
 
+
 	renderer.CreateWindow(640, 480, "Raptor Unleashed");
+
+	// Limit the framerate to the vsync
+	glfwSwapInterval(1);
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	// Sets the background color (values are normalized)
 	glClearColor(.8, 0, .8, 1);
